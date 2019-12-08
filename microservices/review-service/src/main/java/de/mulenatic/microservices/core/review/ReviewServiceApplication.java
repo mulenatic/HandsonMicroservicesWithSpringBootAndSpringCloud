82993c0a6daa6a915dbf911fc1174ef674a8d10c
user <user>
2019-12-08 15:09:55 +0000

    Added the ComponentScan in all microservices, so that api and util can be used.

--- microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package de.mulenatic.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("de.mulenatic")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
