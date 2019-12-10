4ab519861524032837bf31d13ae556c5f31b7190
user <user>
2019-12-10 09:11:10 +0000

    Added script to build all projects.

---
+++ buildProjects.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo 'No parameters given'
+    exit
+fi
+
+for i in api/build.gradle util/build.gradle
+do
+    echo "Adding version in $i";
+    sed -i 's/id '\''io.spring.dependency-management'\'' \/\/version/id '\''io.spring.dependency-management'\'' version/g' $i;
+done
+
+./gradlew $@;
+
+for i in api/build.gradle util/build.gradle
+do
+    echo "Removing version in $i";
+    sed -i 's/id '\''io.spring.dependency-management'\'' version/id '\''io.spring.dependency-management'\'' \/\/version/g' $i;
+done
