513747fca882fd11ab890fa5361be71c4dd989f8
user <user>
2019-12-10 11:02:53 +0000

    Added the Product, Review and Recommendation Service implementations.

--- api/src/main/java/de/mulenatic/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/de/mulenatic/api/core/recommendation/RecommendationService.java
@@ -3,7 +3,6 @@ package de.mulenatic.api.core.recommendation;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RecommendationService {
