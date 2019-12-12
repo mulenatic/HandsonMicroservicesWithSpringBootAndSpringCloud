bfb67ea400e4d23be30714a3ceb40a9a9d660e93
user <user>
2019-12-12 14:27:55 +0000

    Improved the tests for the Review and Recommendation service.

--- microservices/recommendation-service/src/test/java/de/mulenatic/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ microservices/recommendation-service/src/test/java/de/mulenatic/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -2,15 +2,96 @@ package de.mulenatic.microservices.core.recommendation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RecommendationServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
 
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void getRecommendationsByProductId() {
+
+	int productId = 1;
+
+	client.get()
+	    .uri("/recommendation?productId=" + productId)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isOk()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.length()").isEqualTo(3)
+	    .jsonPath("$[0].productId").isEqualTo(productId);
+    }
+
+    @Test
+    public void getRecommendationsMissingParameter() {
+
+	client.get()
+	    .uri("/recommendation")
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/recommendation")
+	    .jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present");
+    }
+
+    @Test
+    public void getRecommendationsInvalidParameter() {
+
+	client.get()
+	    .uri("/recommendation?productId=no-integer")
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/recommendation")
+	    .jsonPath("$.message").isEqualTo("Type mismatch.");
+    }
+    @Test
+    public void getRecommendationsNotFound() {
+
+	int productIdNotFound = 113;
+
+	client.get()
+	    .uri("/recommendation?productId=" + productIdNotFound)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isOk()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.length()").isEqualTo(0);
+    }
+
+    @Test
+    public void getRecommendationsInvalidParameterNegativeValue() {
+
+	int productIdInvalid = -1;
+
+	client.get()
+	    .uri("/recommendation?productId=" + productIdInvalid)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/recommendation")
+	    .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+    }
 }
--- microservices/review-service/src/test/java/de/mulenatic/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/de/mulenatic/microservices/core/review/ReviewServiceApplicationTests.java
@@ -2,15 +2,101 @@ package de.mulenatic.microservices.core.review;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointHttpMethod;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ReviewServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
+    
+    @Test
+    public void contextLoads() {
+    }
+
+
+    @Test
+    public void getReviewsByProductId() {
+
+	int productId = 1;
+
+	client.get()
+	    .uri("/review?productId=" + productId)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isOk()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.length()").isEqualTo(3)
+	    .jsonPath("$[0].productId").isEqualTo(productId);
+    }
+
+    @Test
+    public void getReviewsMissingParameter() {
+
+	client.get()
+	    .uri("/review")
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/review")
+	    .jsonPath("$.message").isEqualTo("Required int parameter 'productId' is not present");
+    }
+
+    @Test
+    public void getReviewsInvalidParameter() {
+
+	client.get()
+	    .uri("/review?productId=no-integer")
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/review")
+	    .jsonPath("$.message").isEqualTo("Type mismatch.");
+    }
+
+    @Test
+    public void getReviewsNotFound() {
+
+	int productIdNotFound = 213;
+
+	client.get()
+	    .uri("/review?productId=" + productIdNotFound)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isOk()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.length()").isEqualTo(0);
+    }
+
+    @Test
+    public void getReviewsInvalidParameterNegativeValue() {
+
+	int productIdInvalid = -1;
+
+	client.get()
+	    .uri("/review?productId=" + productIdInvalid)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/review")
+	    .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+    }
+    
 
 }
