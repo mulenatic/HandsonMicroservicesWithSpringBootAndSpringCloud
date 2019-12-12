fc60b29087e95e336ba43151e9b1821c6869f2f5
user <user>
2019-12-12 13:39:14 +0000

    Improved the tests for the product service.

--- microservices/product-service/src/test/java/de/mulenatic/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/de/mulenatic/microservices/core/product/ProductServiceApplicationTests.java
@@ -2,15 +2,86 @@ package de.mulenatic.microservices.core.product;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ProductServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    public void contextLoads() {
+    }
+
+
+    @Test
+    public void getProductById() {
+
+	int productId = 1;
+
+	client.get()
+	    .uri("/product/" + productId)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isOk()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.productId").isEqualTo(productId);
+    }
+
+    @Test
+    public void getProductInvalidParameterString() {
+
+	client.get()
+	    .uri("/product/no-integer")
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/product/no-integer")
+	    .jsonPath("$.message").isEqualTo("Type mismatch.");
+    }
+
+    @Test
+    public void getProductNotFound() {
+
+	int productIdNotFound = 13;
+
+	client.get()
+	    .uri("/product/" + productIdNotFound)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isNotFound()
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/product/" + productIdNotFound)
+	    .jsonPath("$.message").isEqualTo("No product found for productId: " + productIdNotFound);
+    }
+
+    @Test
+    public void getProductInvalidParameterNegativeValue() {
+
+	int productIdInvalid = -1;
+
+	client.get()
+	    .uri("/product/" + productIdInvalid)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody()
+	    .jsonPath("$.path").isEqualTo("/product/" + productIdInvalid)
+	    .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+    }
+    
 
 }
