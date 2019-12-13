da03595a719ae9817152fcb9472348e9029c00bd
user <user>
2019-12-13 13:38:03 +0000

    Added documentation to the composite api class and corresponding properties.

--- api/src/main/java/de/mulenatic/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/de/mulenatic/api/composite/product/ProductCompositeService.java
@@ -3,6 +3,12 @@ package de.mulenatic.api.composite.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@Api(description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
     /**
@@ -11,8 +17,17 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
+    @ApiOperation(
+		  value = "${api.product-composite.get-composite-product.description}",
+		  notes = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+	    @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+	    @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+	    @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+	})
     @GetMapping(
-        value    = "/product-composite/{productId}",
-        produces = "application/json")
+		value    = "/product-composite/{productId}",
+		produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
+    
 }
