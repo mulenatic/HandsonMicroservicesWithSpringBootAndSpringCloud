8d3eebc0ab9b1f97c12de89da4fc63f65c16d627
user <user>
2019-12-14 11:10:15 +0000

    Added unit Tests for the persistence. Bugfixed the review persistence.

--- microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/persistence/ReviewEntity.java
+++ microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/persistence/ReviewEntity.java
@@ -11,11 +11,10 @@ import javax.persistence.Version;
  * ReviewEntity
  */
 @Entity
-@Table(name = "reviews", indexes = { @Index( name = "reviews_unique_idx", unique = true, columnList = "productId,reviewId" ) })
+@Table(name = "reviews", indexes = { @Index(name = "reviews_unique_idx", unique = true, columnList = "productId,reviewId") }) 
 public class ReviewEntity {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private int id;
 
     @Version
@@ -27,6 +26,10 @@ public class ReviewEntity {
     private String subject;
     private String content;
 
+    public ReviewEntity() {
+
+    }
+
     public ReviewEntity(int productId, int reviewId, String author, String subject, String content) {
 	this.productId = productId;
 	this.reviewId = reviewId;
@@ -35,61 +38,61 @@ public class ReviewEntity {
 	this.content = content;
     }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+	return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public int getVersion() {
-		return version;
-	}
+    public int getVersion() {
+	return version;
+    }
 
-	public void setVersion(int version) {
-		this.version = version;
-	}
+    public void setVersion(int version) {
+	this.version = version;
+    }
 
-	public int getProductId() {
-		return productId;
-	}
+    public int getProductId() {
+	return productId;
+    }
 
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
+    public void setProductId(int productId) {
+	this.productId = productId;
+    }
 
-	public int getReviewId() {
-		return reviewId;
-	}
+    public int getReviewId() {
+	return reviewId;
+    }
 
-	public void setReviewId(int reviewId) {
-		this.reviewId = reviewId;
-	}
+    public void setReviewId(int reviewId) {
+	this.reviewId = reviewId;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public String getAuthor() {
+	return author;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public void setAuthor(String author) {
+	this.author = author;
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    public String getSubject() {
+	return subject;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    public void setSubject(String subject) {
+	this.subject = subject;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+	return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+	this.content = content;
+    }
 
     
 }
--- microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/persistence/ReviewRepository.java
+++ microservices/review-service/src/main/java/de/mulenatic/microservices/core/review/persistence/ReviewRepository.java
@@ -8,7 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * ReviewRepository
  */
-public interface ReviewRepository extends CrudRepository<ReviewEntity, String> {
+public interface ReviewRepository extends CrudRepository<ReviewEntity, Integer> {
 
     @Transactional(readOnly = true)
     List<ReviewEntity> findByProductId(int productId);
