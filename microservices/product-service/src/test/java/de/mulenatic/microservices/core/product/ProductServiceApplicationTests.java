2d05cd3e8c28f16c3250c9375b226cdc4c9f1f13
user <user>
2019-12-16 10:00:53 +0000

    Added tests for the new internal api methods.

--- microservices/product-service/src/test/java/de/mulenatic/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/de/mulenatic/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,5 +1,9 @@
 package de.mulenatic.microservices.core.product;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +14,53 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import de.mulenatic.api.core.product.Product;
+import de.mulenatic.microservices.core.product.persistence.ProductRepository;
+import reactor.core.publisher.Mono;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {"spring.data.mongodb.port: 0"})
 public class ProductServiceApplicationTests {
 
     @Autowired
     private WebTestClient client;
 
+    @Autowired
+    private ProductRepository repository;
+
+    @Before
+    public void setupDb() {
+	repository.deleteAll();
+    }
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void duplicateError() {
+	int productId = 1;
+	postAndVerifyProduct(productId, HttpStatus.OK);
+	assertTrue(repository.findByProductId(productId).isPresent());
+
+	postAndVerifyProduct(productId, HttpStatus.UNPROCESSABLE_ENTITY)
+	    .jsonPath("$.path").isEqualTo("/product")
+	    .jsonPath("$.message").isEqualTo("Duplicate key, Product id: " + productId);
+
+    }
+
+    @Test
+    public void deleteProduct() {
+
+	int productId = 1;
+	postAndVerifyProduct(productId, HttpStatus.OK);
+	assertTrue(repository.findByProductId(productId).isPresent());
+	
+	deleteAndVerifyProduct(productId, HttpStatus.OK);
+	assertFalse(repository.findByProductId(productId).isPresent());
+
+	deleteAndVerifyProduct(productId, HttpStatus.OK);
+
     }
 
 
@@ -82,6 +124,38 @@ public class ProductServiceApplicationTests {
 	    .jsonPath("$.path").isEqualTo("/product/" + productIdInvalid)
 	    .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
     }
+
+    private WebTestClient.BodyContentSpec getAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+	return getAndVerifyProduct("/" + productId, expectedStatus);
+    }
+
+    private WebTestClient.BodyContentSpec getAndVerifyProduct(String productIdPath, HttpStatus expectedStatus) {
+	return client.get()
+	    .uri("/product" + productIdPath)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(expectedStatus)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody();
+    }
+
+    private WebTestClient.BodyContentSpec postAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+
+	Product product = new Product(productId, "Name " + productId, productId, "SA");
+	return client.post().uri("/product").body(Mono.just(product), Product.class)
+	    .accept(MediaType.APPLICATION_JSON_UTF8).exchange().expectStatus().isEqualTo(expectedStatus)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8).expectBody();
+
+    }
+
+    private WebTestClient.BodyContentSpec deleteAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+	return client.delete()
+	    .uri("/product/" + productId)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(expectedStatus)
+	    .expectBody();
+    }  
     
 
 }
--- microservices/review-service/src/test/java/de/mulenatic/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/de/mulenatic/microservices/core/review/ReviewServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package de.mulenatic.microservices.core.review;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.endpoint.web.WebEndpointHttpMethod;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
@@ -11,18 +13,33 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import de.mulenatic.api.core.review.Review;
+import de.mulenatic.microservices.core.review.persistence.ReviewRepository;
+import reactor.core.publisher.Mono;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT, properties = {"spring.datasource.url=jdbc:h2:mem:review-db"})
+
 public class ReviewServiceApplicationTests {
 
     @Autowired
     private WebTestClient client;
+
+    @Autowired
+    private ReviewRepository repository;
+
+    @Before
+    public void setupDb() {
+	repository.deleteAll();
+    }
+
+
     
     @Test
     public void contextLoads() {
     }
 
-
+    
     @Test
     public void getReviewsByProductId() {
 
@@ -38,6 +55,43 @@ public class ReviewServiceApplicationTests {
 	    .jsonPath("$.length()").isEqualTo(3)
 	    .jsonPath("$[0].productId").isEqualTo(productId);
     }
+
+    @Test
+    public void duplicateError() {
+
+	int productId = 1;
+	int reviewId = 1;
+
+	assertEquals(0, repository.count());
+
+	postAndVerifyReview(productId, reviewId, HttpStatus.OK)
+	    .jsonPath("$.productId").isEqualTo(productId)
+	    .jsonPath("$.reviewId").isEqualTo(reviewId);
+
+	assertEquals(1, repository.count());
+
+	postAndVerifyReview(productId, reviewId, HttpStatus.UNPROCESSABLE_ENTITY)
+	    .jsonPath("$.path").isEqualTo("/review")
+	    .jsonPath("$.message").isEqualTo("Duplicate key, Product Id: 1, Review Id:1");
+
+	assertEquals(1, repository.count());
+    }
+
+    @Test
+    public void deleteReviews() {
+
+	int productId = 1;
+	int recommendationId = 1;
+
+	postAndVerifyReview(productId, recommendationId, HttpStatus.OK);
+	assertEquals(1, repository.findByProductId(productId).size());
+
+	deleteAndVerifyReviewsByProductId(productId, HttpStatus.OK);
+	assertEquals(0, repository.findByProductId(productId).size());
+
+	deleteAndVerifyReviewsByProductId(productId, HttpStatus.OK);
+    }
+
 
     @Test
     public void getReviewsMissingParameter() {
@@ -97,6 +151,42 @@ public class ReviewServiceApplicationTests {
 	    .jsonPath("$.path").isEqualTo("/review")
 	    .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
     }
+
+    private WebTestClient.BodyContentSpec getAndVerifyReviewsByProductId(int productId, HttpStatus expectedStatus) {
+	return getAndVerifyReviewsByProductId("?productId=" + productId, expectedStatus);
+    }
+
+    private WebTestClient.BodyContentSpec getAndVerifyReviewsByProductId(String productIdQuery, HttpStatus expectedStatus) {
+	return client.get()
+	    .uri("/review" + productIdQuery)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(expectedStatus)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody();
+    }
+
+    private WebTestClient.BodyContentSpec postAndVerifyReview(int productId, int reviewId, HttpStatus expectedStatus) {
+
+	Review review = new Review(productId, reviewId, "Author " + reviewId, "Subject " + reviewId, "Content " + reviewId, "SA");
+	return client.post()
+	    .uri("/review")
+	    .body(Mono.just(review), Review.class)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(expectedStatus)
+	    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+	    .expectBody();
+    }
+
     
 
+    private WebTestClient.BodyContentSpec deleteAndVerifyReviewsByProductId(int productId, HttpStatus expectedStatus) {
+	return client.delete()
+	    .uri("/review?productId=" + productId)
+	    .accept(MediaType.APPLICATION_JSON_UTF8)
+	    .exchange()
+	    .expectStatus().isEqualTo(expectedStatus)
+	    .expectBody();
+    }
 }
