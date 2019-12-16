3d55951cb8ccfad5dc41989cf269825b2e7d0299
user <user>
2019-12-16 07:11:51 +0000

    Implemented the new methods of the product service api.
    Also had to add setters in the Product DTO and a Mapper for Db objects
    to DTOs (using MapStruct).

--- api/src/main/java/de/mulenatic/api/core/product/Product.java
+++ api/src/main/java/de/mulenatic/api/core/product/Product.java
@@ -2,40 +2,55 @@ package de.mulenatic.api.core.product;
 
 public class Product {
 
-	private final int productId;
-	private final String name;
-	private final int weight;
-	private final String serviceAddress;
+    private  int productId;
+    private  String name;
+    private  int weight;
+    private  String serviceAddress;
 
-    	public Product() {
-		this.productId = 0;
-		this.name = null;
-		this.weight = 0;
-		this.serviceAddress = null;
-	}
+    public Product() {
+	this.productId = 0;
+	this.name = null;
+	this.weight = 0;
+	this.serviceAddress = null;
+    }
 
-	public Product(int productId, String serviceAddress, int weight, String name) {
-		this.productId = productId;
-		this.serviceAddress = serviceAddress;
-		this.weight = weight;
-		this.name = name;
-	}
+    public Product(int productId, String serviceAddress, int weight, String name) {
+	this.productId = productId;
+	this.serviceAddress = serviceAddress;
+	this.weight = weight;
+	this.name = name;
+    }
 
-	public String getServiceAddress() {
-		return serviceAddress;
-	}
+    public String getServiceAddress() {
+	return serviceAddress;
+    }
 
-	public int getProductId() {
-		return productId;
-	}
+    public int getProductId() {
+	return productId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public int getWeight() {
-		return weight;
-	}
+    public int getWeight() {
+	return weight;
+    }
 
+    public void setProductId(int productId) {
+	this.productId = productId;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public void setWeight(int weight) {
+	this.weight = weight;
+    }
+
+    public void setServiceAddress(String serviceAddress) {
+	this.serviceAddress = serviceAddress;
+    }
 
 }
--- microservices/product-service/src/main/java/de/mulenatic/microservices/core/product/services/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/de/mulenatic/microservices/core/product/services/ProductServiceImpl.java
@@ -3,11 +3,14 @@ package de.mulenatic.microservices.core.product.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.mulenatic.api.core.product.Product;
 import de.mulenatic.api.core.product.ProductService;
+import de.mulenatic.microservices.core.product.persistence.ProductEntity;
+import de.mulenatic.microservices.core.product.persistence.ProductRepository;
 import de.mulenatic.util.exceptions.InvalidInputException;
 import de.mulenatic.util.exceptions.NotFoundException;
 import de.mulenatic.util.http.ServiceUtil;
@@ -21,24 +24,56 @@ public class ProductServiceImpl implements ProductService {
     private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
 
     private final ServiceUtil serviceUtil;
+    private final ProductRepository repository;
+    private final ProductMapper mapper;
 
     @Autowired
-    public ProductServiceImpl(ServiceUtil serviceUtil) {
+    public ProductServiceImpl(ServiceUtil serviceUtil, ProductRepository repository, ProductMapper mapper) {
 	this.serviceUtil = serviceUtil;
+	this.repository = repository;
+	this.mapper = mapper;
     }
 
+    @Override
+    public Product createProduct(Product body) {
+
+	try {
+	    ProductEntity entity = mapper.apiToEntity(body);
+	    ProductEntity newEntity = repository.save(entity);
+
+	    return mapper.entityToApi(newEntity);
+	} catch (DuplicateKeyException dke) {
+	    throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId());
+	}
+	  
+    }
+
+    
     
 
     @Override
     public Product getProduct(@PathVariable int productId) {
-	LOG.debug("/product return the found product for productId={}", productId);
 
-	if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+	if (productId < 1 ) throw new InvalidInputException("Invalid productId: " + productId);
 
-	if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
-	
-	return new Product(productId, serviceUtil.getServiceAddress(), 123, "name-" + productId);
+	ProductEntity entity = repository.findByProductId(productId)
+	    .orElseThrow(() -> new NotFoundException("No product found for productId: " + productId));
+	Product response = mapper.entityToApi(entity);
+	response.setServiceAddress(serviceUtil.getServiceAddress());
+
+	return response;
+
+
     }
+
+    @Override
+    public void deleteProduct(int productId) {
+	repository.findByProductId(productId).ifPresent(e -> repository.delete(e));		
+    }
+
+    
+
+
 
 
 
