9ba0a18a14fde67045f3e43a73c9b9f4b45dd90e
user <user>
2019-12-16 11:19:29 +0000

    Implementing the new methods in CompositeServiceImpl.

--- api/src/main/java/de/mulenatic/api/composite/product/RecommendationSummary.java
+++ api/src/main/java/de/mulenatic/api/composite/product/RecommendationSummary.java
@@ -5,13 +5,16 @@ public class RecommendationSummary {
     private final int recommendationId;
     private final String author;
     private final int rate;
+    private final String content;
 
-    public RecommendationSummary(int recommendationId, String author, int rate) {
-        this.recommendationId = recommendationId;
-        this.author = author;
-        this.rate = rate;
+    public RecommendationSummary(int recommendationId, String author, int rate, String content) {
+	this.recommendationId = recommendationId;
+	this.author = author;
+	this.rate = rate;
+	this.content = content;
     }
 
+    
     public int getRecommendationId() {
         return recommendationId;
     }
@@ -23,4 +26,11 @@ public class RecommendationSummary {
     public int getRate() {
         return rate;
     }
+
+    public String getContent() {
+	return content;
+    }
+
+
+    
 }
