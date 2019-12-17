4ccc49a3e28fe08f72d8d15a6e7989d68cee766e
user <user>
2019-12-17 14:33:24 +0000

    Added MessageProcessessors for the three internal microservices.

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
@@ -1,7 +1,6 @@
 package se.magnus.microservices.core.product.persistence;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
 import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveCrudRepository<ProductEntity, String> {
--- microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
+++ microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -1,7 +1,6 @@
 package se.magnus.microservices.core.recommendation.persistence;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
 import reactor.core.publisher.Flux;
 
 public interface RecommendationRepository extends ReactiveCrudRepository<RecommendationEntity, String> {
