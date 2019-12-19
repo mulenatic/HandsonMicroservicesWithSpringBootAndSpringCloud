d8874354b590fbd181094c9f0e8e84da1fd4d6ea
user <user>
2019-12-19 14:03:35 +0000

    Performed all changes necessary to use eureka.

--- api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
@@ -1,27 +1,10 @@
 package se.magnus.api.core.recommendation;
 
 import org.springframework.web.bind.annotation.*;
-
 import reactor.core.publisher.Flux;
-
-import java.util.List;
 
 public interface RecommendationService {
 
-    /**
-     * Sample usage:
-     *
-     * curl -X POST $HOST:$PORT/recommendation \
-     *   -H "Content-Type: application/json" --data \
-     *   '{"productId":123,"recommendationId":456,"author":"me","rate":5,"content":"yada, yada, yada"}'
-     *
-     * @param body
-     * @return
-     */
-    @PostMapping(
-        value    = "/recommendation",
-        consumes = "application/json",
-        produces = "application/json")
     Recommendation createRecommendation(@RequestBody Recommendation body);
 
     /**
@@ -37,13 +20,5 @@ public interface RecommendationService {
         produces = "application/json")
     Flux<Recommendation> getRecommendations(@RequestParam(value = "productId", required = true) int productId);
 
-    /**
-     * Sample usage:
-     *
-     * curl -X DELETE $HOST:$PORT/recommendation?productId=1
-     *
-     * @param productId
-     */
-    @DeleteMapping(value = "/recommendation")
     void deleteRecommendations(@RequestParam(value = "productId", required = true)  int productId);
 }
