d0e6f287120787771925e75f33ef9fb84dbf57cb
user <user>
2020-01-03 06:52:06 +0000

    Removed eureka.client.enabled property from clients tests.

--- microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
@@ -28,7 +28,6 @@ import static se.magnus.api.event.Event.Type.DELETE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=RANDOM_PORT, properties = {
-	"eureka.client.enabled=false",
 	"spring.cloud.config.enabled=false",
     "spring.datasource.url=jdbc:h2:mem:review-db"})
 public class ReviewServiceApplicationTests {
